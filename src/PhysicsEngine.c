5adc64f7398254d62c77a5fce4bc09490b7e3a7a
lukasmellemrumkluge <39825381+lukasmellemrumkluge@users.noreply.github.com>
2018-06-20 13:53:35 +0200

    just a little more cleansing

--- src/PhysicsEngine.c
+++ src/PhysicsEngine.c
@@ -313,7 +313,8 @@ void updateStrikers(uint32_t * striker0_p, uint32_t * striker1_p) {
 
 
 //Checks all collisions of all balls, updates accordingly.
-void updatePhysics(ball_t * ball_p, uint8_t * activeBalls_p, uint32_t * striker0_p, uint32_t * striker1_p, uint8_t * lives_p, uint16_t * score_p, uint32_t * bricks_p, uint32_t * specialBricks_p){
+void updatePhysics(ball_t * ball_p, uint8_t * activeBalls_p, uint32_t * striker0_p,
+	uint32_t * striker1_p, uint8_t * lives_p, uint16_t * score_p, uint32_t * bricks_p, uint32_t * specialBricks_p){
 
     uint8_t i;
 
